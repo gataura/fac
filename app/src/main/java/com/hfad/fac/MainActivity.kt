1aa3434904ddcc84b151a5fdaade49cec50724c6
salikh <k612nn@gmail.com>
2018-11-22 15:52:31 +0300

    вторая попытка, делаю теперь через webView

--- app/src/main/java/com/hfad/fac/MainActivity.kt
+++ app/src/main/java/com/hfad/fac/MainActivity.kt
@@ -2,11 +2,18 @@ package com.hfad.fac
 
 import android.content.Context
 import android.content.Intent
+import android.content.Intent.getIntent
 import android.net.Uri
+import android.os.Build
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.support.annotation.RequiresApi
+import android.support.v4.content.ContextCompat
+import android.support.v4.content.ContextCompat.startActivity
 import android.view.View
+import android.webkit.WebResourceRequest
 import android.webkit.WebView
+import android.webkit.WebViewClient
 import android.widget.Toast
 import com.hfad.fac.api.model.AccessToken
 import com.hfad.fac.api.model.GitHubRepo
@@ -21,10 +28,11 @@ import retrofit2.Response
 import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
 
+@Suppress("OverridingDeprecatedMember")
 class MainActivity : AppCompatActivity() {
 
 
-
+    var webView: WebView? = null
     var clientId = "34f18331adf3a2ecc7b3"
     var clientSecret = "9299ce3bfd5e19e1b894871d0680ba38a94a0483"
     var redirectUri = "fac://callback"
@@ -33,14 +41,24 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+        webView = findViewById<WebView>(R.id.webView) as WebView
+        webView!!.settings.javaScriptEnabled = true
+        webView!!.webViewClient = object : WebViewClient() {
+            override fun shouldOverrideUrlLoading(view: WebView?, url: String?): Boolean {
+                OAuth()
+                return true
+            }
 
-        val intent = Intent (Intent.ACTION_VIEW, Uri.parse("https://github.com/login/oauth/authorize?client_id=$clientId&scope=repo&redirect_uri=$redirectUri"))
-        startActivity(intent)
+            @RequiresApi(Build.VERSION_CODES.N)
+            override fun shouldOverrideUrlLoading(view: WebView?, request: WebResourceRequest?): Boolean {
+                OAuth()
+                return true
+            }
 
-    }
+        }
 
 
-    override fun onResume() {
+        /*override fun onResume() {
         super.onResume()
         var uri = getIntent().getData()
 
@@ -71,5 +89,38 @@ class MainActivity : AppCompatActivity() {
             })
         }
 
+    }*/
+    }
+    fun OAuth() {
+        val intent = Intent(Intent.ACTION_VIEW, Uri.parse("https://github.com/login/oauth/authorize?client_id=$clientId&scope=repo&redirect_uri=$redirectUri"))
+        startActivity(intent)
+        var uri = getIntent().getData()
+
+        if (uri != null && uri.toString().startsWith(redirectUri)) {
+            var code = uri.getQueryParameter("code")
+
+
+            var builder = Retrofit.Builder().baseUrl("https://github.com/").addConverterFactory(GsonConverterFactory.create())
+
+            var retrofit: Retrofit = builder.build()
+
+            var client = retrofit.create(GitHubClient::class.java)
+            var accessTokenCall = client.getAccessToken(
+                    clientId,
+                    clientSecret,
+                    code
+            )
+
+            accessTokenCall.enqueue(object : Callback<AccessToken> {
+                override fun onResponse(call: Call<AccessToken>?, response: Response<AccessToken>?) {
+                    Toast.makeText(this@MainActivity, "yay!", Toast.LENGTH_SHORT).show()
+                }
+
+                override fun onFailure(call: Call<AccessToken>?, t: Throwable?) {
+                    Toast.makeText(this@MainActivity, "no!", Toast.LENGTH_SHORT).show()
+                }
+
+            })
+        }
     }
 }
